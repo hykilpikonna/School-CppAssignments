9ff1271f28689b2fd17ae5df8ac8ed897dde15b2
Hykilpikonna <me@hydev.org>
2020-12-07 20:53:08 -0500

    [+] Create macros var, val, and let

--- projects/p02-conway/macros.h
+++ projects/p02-conway/macros.h
@@ -16,12 +16,14 @@
 
 using namespace std;
 
-// Terrible C stuff: int is only 16-bits on LP32
-#define Int uint32_t
-#define Long uint64_t
-
 // Kotlin habit ;-;
+#define Int uint32_t // int is only 16-bits on LP32
+#define Long uint64_t
+#define List vector
 #define HashMap unordered_map
 #define String string
+#define var auto
+#define let auto
+#define val const auto
 
 #endif //HOMEWORKS_MACROS_H
