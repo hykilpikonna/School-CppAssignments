a803ca6e4e466b1ce99e84ff716ca809d592a4af
Hykilpikonna <me@hydev.org>
2020-12-08 15:47:14 -0500

    [+] Implement isCoin(x,y)

--- projects/p02-conway/BoardWithCoins.cpp
+++ projects/p02-conway/BoardWithCoins.cpp
@@ -4,6 +4,13 @@
 
 #include "BoardWithCoins.h"
 
+bool BoardWithCoins::isCoin(Int x, Int y)
+{
+    if (x < 0 || x > w) return false;
+    if (y < 0 || y > h) return false;
+    return binary_search(coins.begin(), coins.end(), pointHash(x, y));
+}
+
 void BoardWithCoins::print()
 {
 
--- projects/p02-conway/BoardWithCoins.h
+++ projects/p02-conway/BoardWithCoins.h
@@ -20,6 +20,15 @@ public:
      */
     BoardWithCoins(Int w, Int h): Board(w, h) {}
 
+    /**
+     * Get whether the cell at a position is a coin. (Assumes `coins` is sorted)
+     *
+     * @param x Position X.
+     * @param y Position Y.
+     * @return Is coin or not.
+     */
+    bool isCoin(Int x, Int y);
+
     void print() override;
 };
 
