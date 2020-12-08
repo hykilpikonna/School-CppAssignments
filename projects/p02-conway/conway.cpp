c207d2356e8a4c6c45f4d897a4f93e6afe28e4e2
Hykilpikonna <me@hydev.org>
2020-12-08 14:10:44 -0500

    [+] Add tests for isAlive

--- projects/p02-conway/conway.cpp
+++ projects/p02-conway/conway.cpp
@@ -20,7 +20,6 @@ int main(int argc, char* argv[])
     // Prompt
     cout << "Welcome to Hykilp's implementation of Conway's Game of Life!\n\n"
             "Launch options:\n"
-            "0: Configure."
             "1: Start simulation.\n"
             "2: Try my challenge!\n";
 
@@ -44,6 +43,24 @@ void runTests()
     assert(pointHash(5, 1) == 21474836481);
     val [x, y] = pointUnhash(21474836481);
     assert(x == 5 && y == 1);
+
+    // Border cases
+    var board = Board(3, 4);
+    for (val x : {0, 1, 2})
+        for (val y : {0, 1, 2, 3})
+            board.alive.emplace_back(pointHash(x, y));
+    sort(board.alive.begin(), board.alive.end());
+    assert(!board.isAlive(0, -1));
+    assert(!board.isAlive(-1, -1));
+    assert(!board.isAlive(-1, 0));
+    assert(!board.isAlive(3, 3));
+    assert(!board.isAlive(2, 4));
+    for (val x : {0, 1, 2})
+        for (val y : {0, 1, 2, 3})
+            assert(board.isAlive(x, y));
+
+    // Done!
+    cout << "All Tests Passed!" << endl;
 }
 
 void startSim(Board board)
