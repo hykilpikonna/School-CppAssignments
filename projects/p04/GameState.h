f78807d29fe911a9d87589188ac17f6f3b2ae8f5
Hykilpikonna <me@hydev.org>
2021-01-18 16:24:15 -0500

    [+] Create checkResult function

--- projects/p04/GameState.h
+++ projects/p04/GameState.h
@@ -9,6 +9,16 @@
 #include "macros.h"
 #include "utils.h"
 
+// Winning combos
+List<List<Int>> combos = {
+        // Horizontal
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+        // Vertical
+        {0, 3, 6}, {1, 4, 7}, {2, 6, 8},
+        // Diagonal
+        {0, 4, 8}, {2, 4, 6}
+};
+
 /**
  * Player interface
  */
@@ -49,6 +59,30 @@ public:
 
         // To next player
         currentPlayer = (currentPlayer + 1) % 2;
+    }
+
+    /**
+     * Check who won, or draw
+     *
+     * @return Game result [-1: there's no result yet, 0: player 1 wins, 1: payer 2 wins, 2: draw)
+     */
+    int checkResult()
+    {
+        // Check if all grid cells are filled TODO: Predict draws
+        for (int n : grid)
+        {
+            if (n == -1) return -1; // No results yet
+        }
+
+        // Check winning combos
+        for (List<Int> combo : combos)
+        {
+            val sum = grid[combo[0]] + grid[combo[1]] + grid[combo[2]];
+            if (sum == 0) return 0; // Player 1 wins
+            if (sum == 3) return 1; // Player 2 wins
+        }
+
+        return 2; // Draws
     }
 };
 
