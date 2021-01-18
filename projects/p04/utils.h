1de57fe0222238acde90f95d14ca29571ba050c5
Hykilpikonna <me@hydev.org>
2021-01-18 16:23:07 -0500

    [+] Create utility functions to convert between xy and loc index

---
+++ projects/p04/utils.h
@@ -0,0 +1,31 @@
+//
+// Created by Hykilpikonna on 1/18/21.
+//
+
+#ifndef SCHOOL_CPP_UTILS_H
+#define SCHOOL_CPP_UTILS_H
+
+/**
+ * Convert x-y position to location index
+ *
+ * @param x
+ * @param y
+ * @return Location index
+ */
+int xyToLoc(int x, int y)
+{
+    return x * rows + y;
+}
+
+/**
+ * Convert location index to x-y position
+ *
+ * @param loc Location index
+ * @return x-y position
+ */
+tuple<int, int> locToXy(int loc)
+{
+    return {loc / rows, loc % rows};
+}
+
+#endif //SCHOOL_CPP_UTILS_H
