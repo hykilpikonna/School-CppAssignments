6b2ebf632d17db32735e3b650cdfb8adb464af0c
Hykilpikonna <me@hydev.org>
2021-01-18 16:24:04 -0500

    [+] Create movePlayer function

--- projects/p04/GameState.h
+++ projects/p04/GameState.h
@@ -35,6 +35,21 @@ public:
         // Init grid (-1: No one placed anything here yet)
         for (int& n : grid) { n = -1; }
     }
+
+    /**
+     * Make a move for the current player
+     *
+     * @param x
+     * @param y
+     */
+    void movePlayer(int x, int y)
+    {
+        // Set grid state
+        grid[xyToLoc(x, y)] = currentPlayer;
+
+        // To next player
+        currentPlayer = (currentPlayer + 1) % 2;
+    }
 };
 
 #endif //SCHOOL_CPP_GAMESTATE_H
