9bba5c6fbffad44d36ec7bee899323bb28d5c4b6
Hykilpikonna <me@hydev.org>
2021-05-19 05:06:08 +0800

    [O] Use minimum c++ version

--- projects/p04/compile.sh
+++ projects/p04/compile.sh
@@ -1,4 +1,4 @@
-# brew install gtkmm4
-g++ main.cpp -o output `pkg-config gtkmm-4.0 --cflags --libs` -std=c++2a
+brew install gtkmm4
+g++ main.cpp -o output `pkg-config gtkmm-4.0 --cflags --libs` -std=c++17
 chmod +x output
 ./output
